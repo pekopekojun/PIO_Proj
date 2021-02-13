69cf566b5853b47545b99b64c76397a5fd0b0ab5
peko_jun <jun805@gmail.com>
2021-02-14 00:03:01 +0900

    Improved : Measurement rate (2ms interval)

--- ina260/src/main.cpp
+++ ina260/src/main.cpp
@@ -17,99 +17,109 @@ static CRGB leds[NUM_LEDS];
 
 void setup()
 {
-  M5.begin(true, true, false);
-  Serial.begin(115200);
-  ina260.begin(26, 32);
-  buff_cnt = 0;
+    M5.begin(true, true, false);
+    Serial.begin(115200);
+    ina260.begin(26, 32);
+    Wire.setClock(400000);
+    buff_cnt = 0;
 
-  FastLED.addLeds<WS2812, LED_PIN>(leds, NUM_LEDS);
-  FastLED.setBrightness(20);
+    FastLED.addLeds<WS2812, LED_PIN>(leds, NUM_LEDS);
+    FastLED.setBrightness(20);
 }
 
 void drawpix(uint32_t i, uint32_t c)
 {
-  leds[i] = c; /*GGRRBB*/
-  FastLED.show();
+    leds[i] = c; /*GGRRBB*/
+    FastLED.show();
 }
 
 void clearpix(void)
 {
-  for (int i = 0; i < NUM_LEDS; i++)
-  {
-    leds[i] = 0;
-  }
-  FastLED.show();
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        leds[i] = 0;
+    }
+    FastLED.show();
 }
 
 void loop()
 {
-  int32_t mv;
-  int32_t ma;
-  int32_t uv;
-  int32_t ua;
-  uint32_t us;
-  uint32_t flush_size = 0;
-  uint32_t old_color = 0;
-  uint32_t new_color = 0;
-
-  while (1)
-  {
-    uv = ina260.getVoltage_uv();
-    ua = ina260.getCurrent_ua() + 2500;
-
-    us_list[buff_cnt] = micros();
-    mv_100_list[buff_cnt] = uv / 100000;
-    ma_list[buff_cnt] = ua / 1000;
-    buff_cnt = (buff_cnt + 1) % BUFF_SIZE;
-    /*GGRRBB*/
-
-    new_color = (buff_cnt * 255) / BUFF_SIZE;
-    if (new_color != old_color)
+    int32_t mv;
+    int32_t ma;
+    int32_t uv;
+    int32_t ua;
+    uint32_t us;
+    uint32_t flush_size = 0;
+    uint32_t old_color = 0;
+    uint32_t new_color = 0;
+    uint32_t start;
+    while (1)
     {
-      drawpix((buff_cnt * 25) / BUFF_SIZE, ((255 - new_color) << 16) | (new_color << 8));
-    }
-    old_color = new_color;
+        start = micros();
+        uv = ina260.getVoltage_uv();
+        ua = ina260.getCurrent_ua() + 2500;
 
-    if (buff_cnt == 0)
-    {
-      flush_size = BUFF_SIZE;
-    }
+        us_list[buff_cnt] = micros();
+        mv_100_list[buff_cnt] = uv / 100000;
+        ma_list[buff_cnt] = ua / 1000;
+        buff_cnt = (buff_cnt + 1) % BUFF_SIZE;
 
-    M5.update();
-    if (M5.Btn.wasPressed())
-    {
-      flush_size = buff_cnt;
-      buff_cnt = 0;
-    }
-
-    if (buff_cnt == 0)
-    {
-      Serial.println("us,mv,ma,#####");
-      for (int i = 0; i < flush_size; i++)
-      {
-        us = us_list[i];
-        mv = mv_100_list[i];
-        ma = ma_list[i];
-
-        Serial.print(us);
-        Serial.print(",");
-        Serial.print(mv / 10);
-        Serial.print(".");
-        Serial.print(mv % 10);
-        Serial.print(",");
-        Serial.print(ma);
-        Serial.print("");
-        Serial.println("");
-
-        new_color = (i * 25) / flush_size;
+        new_color = (buff_cnt * 255) / BUFF_SIZE;
         if (new_color != old_color)
         {
-          drawpix(25 - new_color, 0);
+            drawpix((buff_cnt * 25) / BUFF_SIZE, ((255 - new_color) << 16) | (new_color << 8));
         }
         old_color = new_color;
-      }
-      clearpix();
+
+        if (buff_cnt == 0)
+        {
+            flush_size = BUFF_SIZE;
+        }
+
+        M5.update();
+        if (M5.Btn.wasPressed())
+        {
+            flush_size = buff_cnt;
+            buff_cnt = 0;
+        }
+
+        if (buff_cnt == 0)
+        {
+            Serial.println("us,mv,ma,#####");
+            for (int i = 0; i < flush_size; i++)
+            {
+                us = us_list[i];
+                mv = mv_100_list[i];
+                ma = ma_list[i];
+
+                Serial.print(us);
+                Serial.print(",");
+                Serial.print(mv / 10);
+                Serial.print(".");
+                Serial.print(mv % 10);
+                Serial.print(",");
+                Serial.print(ma);
+                Serial.print("");
+                Serial.println("");
+
+                new_color = (i * 25) / flush_size;
+                if (new_color != old_color)
+                {
+                    drawpix(25 - new_color, 0);
+                }
+                old_color = new_color;
+            }
+            clearpix();
+        }
+        else
+        {
+            uint32_t now = micros();
+            if ((now - start) < 2000)
+            {
+                delayMicroseconds(2000 - (now - start) );
+            }
+
+            //Serial.println(micros() - start);
+        }
     }
-    //delayMicroseconds(100);
-  }
 }
